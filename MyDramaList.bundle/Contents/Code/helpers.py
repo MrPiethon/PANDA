40b3b52fab9e22d8f8103e227bd51fe9fe84f991
Masemune <masemunejp@gmail.com>
2020-05-03 22:17:33 -0700

    1

--- MyDramaList.bundle/Contents/Code/helpers.py
+++
@@ -1,7 +0,0 @@
-# This builtin function doesn't seem to exist in RestrictedPython
-# implementation from https://docs.python.org/2.7/library/functions.html#any
-def any(iterable):
-    for element in iterable:
-        if element:
-            return True
-    return False
